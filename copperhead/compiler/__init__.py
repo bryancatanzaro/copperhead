98f5d429d3eb0cf47fc70176e4a90bacf1e8b22d
Bryan Catanzaro <catanzar@cs.berkeley.edu>
2011-11-11 22:11:49 -0800

    Added mechanism to load in compiled binaries.

--- copperhead/compiler/__init__.py
+++ copperhead/compiler/__init__.py
@@ -1,17 +1,14 @@
-#
-#  Copyright 2008-2010 NVIDIA Corporation
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-#
+import imp as _imp
+import os as _os
+import glob as _glob
+_cur_dir, _cur_file = _os.path.split(__file__)
 
-# Empty
+def _find_module(name):
+    _ext_poss = _glob.glob(_os.path.join(_cur_dir, name+'*'))
+    if len(_ext_poss) != 1:
+        raise ImportError(name)
+    return _imp.load_dynamic(name, _os.path.join(_cur_dir, _ext_poss[0]))
+
+compiler = _find_module('compiler')
+coresyntax = _find_module('coresyntax')
+coretypes = _find_module('coretypes')
