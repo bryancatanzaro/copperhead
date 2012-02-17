d3b9da24a4f4c96973f4e5e1747e5883c3ee7708
serge_sans_paille <serge.guelton@telecom-bretagne.eu>
2012-02-17 09:27:10 +0100

    Better integration to setup.py
    Rely on build_ext to forward calls to scons instead of blindly calling it.
    Forward `python setup.py clean' to scons for proper cleaning step
    Rely on setup.py for tests.
    
    It is now possible to run
    	`python setup.py test' to test the package.
    	`python setup.py clean' to clean the package.
    
    --HG--
    extra : source : 5ef3a3f3c843055ca89eae9b3f03933b8f37817d

--- setup.py
+++ setup.py
@@ -17,52 +17,79 @@
 # 
 #
 
-import distribute_setup
-distribute_setup.use_setuptools()
+from distribute_setup import use_setuptools
+use_setuptools()
 
 from setuptools import setup
 
-#Call custom build routines to create Python extensions
+from distutils.errors import CompileError
+
+from distutils.command.build_ext import build_ext as BuildExtCommand
+from distutils.command.clean import clean as CleanCommand
+from distutils.cmd import Command
+
 import subprocess
-try:
-    subprocess.check_call(['scons'], shell=True)
-except subprocess.CalledProcessError as e:
-    from distutils.errors import CompileError
-    raise CompileError("Error while building Python Extensions")
+
+# Call custom build routines to create Python extensions
+class CopperheadBuildExt(Command):
+    user_options=[]
+    description = BuildExtCommand.description
+
+    def initialize_options(self):pass
+    def finalize_options(self):pass
+    def get_source_files(self): return []
+
+    def run(self):
+        try:
+            subprocess.check_call(['scons'], shell=True)
+        except subprocess.CalledProcessError:
+            raise CompileError("Error while building Python Extensions")
+        self.extensions=[]
+
+# Call custom clean command to forward call to SCons
+class CopperheadClean(CleanCommand):
+
+    def run(self):
+        CleanCommand.run(self)
+        try:
+             subprocess.check_call(['scons', '--remove'])
+        except subprocess.CalledProcessError:
+            raise CompileError("Error while cleaning Python Extensions")
+
+##
 
 setup(name="copperhead",
-      version="0.2a1",
-      description="Data Parallel Python",
-      long_description="""
-      Copperhead is a Data Parallel Python dialect, with runtime code
-      generation and execution for CUDA Graphics Processors.
-      """,
-      classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python',
-        'Topic :: Software Development :: Compilers',
-        'Topic :: Software Development :: Code Generators',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: MacOS :: MacOS X'],
-      zip_safe=False,
-      author="Bryan Catanzaro, Michael Garland",
-      author_email="bcatanzaro@nvidia.com, mgarland@nvidia.com",
-      license = "Apache 2.0",
-      package_dir = {'':'stage'},   # packages are under stage
-      packages=['copperhead', 'copperhead.runtime', 'copperhead.compiler'],
-      install_requires=[
-        "codepy>=2012.1.1"],
-      package_data={
-        'copperhead': ['library/*/*.h'],
-        'copperhead.compiler' : ['backendcompiler.so',
-                                 'backendsyntax.so',
-                                 'backendtypes.so'],
-        'copperhead.runtime' : ['cudata.so', 'load.so', 'cuda_info.so',
-                                'libcunp.*','libcopperhead.*']
-        },
-      url="http://code.google.com/p/copperhead",
-      )
+        version="0.2a1",
+        description="Data Parallel Python",
+        long_description="Copperhead is a Data Parallel Python dialect, with runtime code generation and execution for CUDA Graphics Processors.",
+        classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: Apache Software License',
+            'Natural Language :: English',
+            'Programming Language :: Python',
+            'Topic :: Software Development :: Compilers',
+            'Topic :: Software Development :: Code Generators',
+            'Operating System :: POSIX :: Linux',
+            'Operating System :: MacOS :: MacOS X'],
+        zip_safe=False,
+        author="Bryan Catanzaro, Michael Garland",
+        author_email="bcatanzaro@nvidia.com, mgarland@nvidia.com",
+        license = "Apache 2.0",
+        package_dir = {'':'stage'},   # packages are under stage
+        packages=['copperhead', 'copperhead.runtime', 'copperhead.compiler'],
+        ext_modules=[('copperhead','')], # the name as no meaning as we override the build_ext command to call SCons
+        install_requires=["codepy>=2012.1.1"],
+        package_data={
+            'copperhead': ['library/*/*.h'],
+            'copperhead.compiler' : ['backendcompiler.so',
+                'backendsyntax.so',
+                'backendtypes.so'],
+            'copperhead.runtime' : ['cudata.so', 'load.so', 'cuda_info.so',
+                'libcunp.*','libcopperhead.*']
+            },
+        url="http://code.google.com/p/copperhead",
+        cmdclass = { 'build_ext' : CopperheadBuildExt, 'clean': CopperheadClean },
+        test_suite = 'copperhead.tests.test_all',
+        )
