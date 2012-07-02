b4333de3685f75305833480176ccb4570dc160b7
Bryan Catanzaro <bcatanzaro@acm.org>
2012-07-02 11:05:15 -0700

    Synchronizing.

--- samples/mandelbrot.py
+++ samples/mandelbrot.py
@@ -11,24 +11,49 @@ def z_magnitude(z):
     return sqrt(real * real + imag * imag)
 
 @cu
-def z_add((z0r, z0i), (z1r, z1i)):
+def z_add(z0, z1):
+    z0r, z0i = z0
+    z1r, z1i = z1
     return z0r + z1r, z0i + z1i
 
 @cu
-def mandelbrot(x, y, m, t):
-    def mandelbrot_iteration(z0, z, i):
-    if i > t:
-        return t
+def mandelbrot_iteration(z0, z, i, m, t):
+    z = z_add(z_square(z), z0)
+    escaped = z_magnitude(z) > m
+    converged = i > t
+    done = escaped or converged
+    if not done:
+        return mandelbrot_iteration(z0, z, i+1, m, t)
     else:
-        z = z_add(z_square(z), z0)
-        if z_magnitude(z) < m:
-            return mandelbrot_iteration(z0, z, i+1, m, t)
-        else:
-            return i
+        return i
 
-    def mandelbrot_el(xi, yi):
-        return mandelbrot_iteration((xi, yi), (xi, yi), 0)
 
-    return map(mandelbrot_el, x, y)
+@cu
+def mandelbrot(lb, scale, (x, y), m, t):
 
-        
+    def mandelbrot_el(zi):
+        return mandelbrot_iteration(zi, zi, 0, m, t)
+
+    def index(i):
+        scale_x, scale_y = scale
+        lb_x, lb_y = lb
+        return float32(i % x) * scale_x + lb_x, float32(i / x) * scale_y + lb_y
+
+    
+    two_d_points = map(index, range(x*y))
+
+    return map(mandelbrot_el, two_d_points)
+    
+
+
+lb = (np.float32(-2.0), np.float32(-2.0))
+x, y = 100, 100
+scale = (np.float32(.04), np.float32(.04))
+
+result = mandelbrot(lb, scale, (x,y), np.float32(4.0), 100)
+
+import matplotlib.pyplot as plt
+
+im_result = to_numpy(result).reshape([x, y])
+plt.imshow(im_result)
+plt.show()
--- tests/test_tail_recursion.py
+++ tests/test_tail_recursion.py
@@ -44,5 +44,31 @@ def count(val, limit):
     #print(infinite_loop(0))
     #print(non_tail_recursive(0))
 
-    #print(anticount(0, 10))
+
+@cu
+def vinc(x, val, limit):
+    if (val == limit):
+        return x
+    else:
+        return vinc(map(lambda xi: xi+1, x), val+1, limit)
+
+print(anticount(0, 10))
 print(count(0, 10))
+#print(vinc([0,0,0], 0, 5))
+
+@cu
+def divergent(limit):
+    def divergent_sub(val):
+        if (val == limit):
+            return val
+        else:
+            return divergent_sub(val+1)
+    return map(divergent_sub, range(limit))
+
+print(divergent(10))
+    
+
+#XXX Things to do:
+# Fix pruning for loop-carried dependences
+# Fix phase inference for loop-carried dependences
+# Fix calling closures recursively
