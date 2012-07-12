72a7f97c90006d79f2604a4852dc32828c05207a
Bryan Catanzaro <bcatanzaro@acm.org>
2012-07-12 16:25:03 -0700

    Simplify code.

--- samples/laplace.py
+++ samples/laplace.py
@@ -7,7 +7,8 @@ dx2 = dx*dx
 dy2 = dy*dy
 
 @cu
-def initialize(nx, ny):
+def initialize(N):
+    nx, ny = N
     def el(i):
         y = i / nx
         if y==0:
@@ -17,7 +18,10 @@ def initialize(nx, ny):
     return map(el, range(nx * nx))
 
 @cu
-def update(u, nx, ny, dx2, dy2):
+def solve(u, N, D2, it):
+    nx, ny = N
+    dx2, dy2 = D2
+    
     def el(i):
         x = i % nx
         y = i / nx
@@ -27,27 +31,22 @@ def update(u, nx, ny, dx2, dy2):
             return ((u[i-1]+u[i+1])*dy2 + \
                         (u[i-nx]+u[i+nx])*dx2)/(2*(dx2+dy2))
         
-    
-    return map(el, indices(u))
-
-@cu
-def solve(u, nx, ny, dx2, dy2, it):    
+            
     if it > 0:
-        u = update(u, nx, ny, dx2, dy2, it)
-        return solve(u, nx, ny, dx2, dy2, it-1)
+        u = map(el, indices(u))
+        return solve(u, N, D2, it-1)
     else:
         return u
     
 
 N = 100
 
-u = initialize(N, N)
+u = initialize((N, N))
 
 p = runtime.places.default_place
-print("Starting timer")
 import time
 start = time.time()
-u = solve(u, N, N, dx2, dy2, 8000)
+u = solve(u, (N, N), (dx2, dy2), 8000)
 #Force result to be finalized at execution place
 u = force(u, p)
 end = time.time()
